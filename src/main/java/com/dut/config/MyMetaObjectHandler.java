2c248b4532533f6993e48c840006597a412ef4dc
zzy77 <15226603631@163.com>
2024-09-05 18:13:08 +0800

    实现商品发布页面.修复无法自动填充时间的bug

--- src/main/java/com/dut/config/MyMetaObjectHandler.java
+++ src/main/java/com/dut/config/MyMetaObjectHandler.java
@@ -2,9 +2,11 @@ package com.dut.config;
 
 import com.baomidou.mybatisplus.core.handlers.MetaObjectHandler;
 import org.apache.ibatis.reflection.MetaObject;
+import org.springframework.stereotype.Component;
 
 import java.time.LocalDateTime;
 
+@Component
 public class MyMetaObjectHandler implements MetaObjectHandler {
     @Override
     public void insertFill(MetaObject metaObject) {
